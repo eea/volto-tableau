f37867f3d07c5dd05b91ea0b758eced631afd458
Miu Razvan <miu.razvan28@gmail.com>
2023-11-10 18:14:34 +0200

    feat: fix tableau not loading on hard reload

--- src/Tableau/Tableau.jsx
+++ src/Tableau/Tableau.jsx
@@ -333,6 +333,7 @@ const Tableau = forwardRef((props, ref) => {
   }, [data]);
 
   useEffect(() => {
+    // console.log('HERE trigger initiateViz', tableau, url);
     if (!tableau) return;
     if (url) {
       disposeViz();
--- src/hooks.js
+++ src/hooks.js
@@ -1,17 +1,33 @@
 import { useEffect, useState } from 'react';
 import { loadTableauScript } from './helpers';
 
+let clock;
+
+const TIMEOUT = 10000;
+
 export const useTableau = (version) => {
   const [tableau, setTableau] = useState();
 
   useEffect(() => {
-    loadTableauScript(() => {
-      if (window[`tableau_${version}`]) {
-        setTableau(window[`tableau_${version}`]);
-      } else {
-        setTableau(undefined);
+    loadTableauScript(version);
+
+    const startTime = new Date().getTime();
+
+    clock = setInterval(() => {
+      const tableauApi = window[`tableau_${version}`];
+      if (tableauApi) {
+        setTableau(tableauApi);
+        clearInterval(clock);
+        return;
       }
-    }, version);
+      if (new Date().getTime() - startTime > TIMEOUT) {
+        clearInterval(clock);
+      }
+    }, 100);
+
+    return () => {
+      clearInterval(clock);
+    };
   }, [version]);
 
   return tableau;

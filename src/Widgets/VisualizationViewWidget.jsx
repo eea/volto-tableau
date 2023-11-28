88a191179be1074688924389fd4303aa3ace4c2f
kreafox <krisztina.n.elekes@gmail.com>
2023-11-28 13:25:59 +0200

    fix: pass static parameters to tableau in widget view

--- src/Widgets/VisualizationViewWidget.jsx
+++ src/Widgets/VisualizationViewWidget.jsx
@@ -1,9 +1,22 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import config from '@plone/volto/registry';
 import { pickMetadata } from '@eeacms/volto-embed/helpers';
 import Tableau from '@eeacms/volto-tableau/Tableau/Tableau';
 
 function VisualizationViewWidget(props) {
+  const { staticParameters = [] } = props.value;
+
+  const extraOptions = React.useMemo(() => {
+    const options = {};
+    staticParameters.forEach((parameter) => {
+      if (parameter.field && parameter.value) {
+        options[parameter.field] = parameter.value;
+      }
+    });
+    return options;
+  }, [staticParameters]);
+
   return (
     <Tableau
       data={{
@@ -16,6 +29,7 @@ function VisualizationViewWidget(props) {
         with_enlarge: true,
         with_download: true,
       }}
+      extraOptions={extraOptions}
       breakpoints={
         config.blocks.blocksConfig?.embed_tableau_visualization?.breakpoints
       }

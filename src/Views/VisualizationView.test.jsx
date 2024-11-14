862b32c6d2760cc2169eef9f900491e6afb9a00a
Miu Razvan <miu.razvan28@gmail.com>
2024-11-14 17:18:54 +0200

    add test

--- src/Views/VisualizationView.test.jsx
+++ src/Views/VisualizationView.test.jsx
@@ -5,10 +5,24 @@ import { Provider } from 'react-redux';
 
 import VisualizationView from './VisualizationView';
 
+jest.doMock('@plone/volto/registry', {
+  blocks: {
+    blocksConfig: {
+      embed_tableau_visualization: {
+        breakpoints: {
+          desktop: [Infinity, 992],
+          tablet: [991, 768],
+          phone: [767, 0],
+        },
+      },
+    },
+  },
+});
+
 describe('VisualizationViewWidget', () => {
   it('should render the component', () => {
     const { container } = render(
-      <Provider store={global.store}>
+      <Provider store={{ ...global.store }}>
         <VisualizationView
           content={{
             tableau_visualization: {
